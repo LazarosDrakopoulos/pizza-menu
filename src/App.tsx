e919f6b022e58c7747ead869a9892271ed16793c
Lazaros Drakopoulos <lazaros.drou@gmail.com>
2026-06-12 18:36:03 +0300

    final fixes

--- src/App.tsx
+++ src/App.tsx
@@ -90,7 +90,7 @@ function App() {
 
   return (
     <div className="app">
-      <h2 className="category-title">Pizzaaa</h2>
+      <h2 className="category-title">Pizza</h2>
       <div className="menu-list">
         {menuState.map(item => (
           <ItemCard
